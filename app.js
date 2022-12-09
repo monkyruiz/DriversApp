8b51c7c855fba3a4f1cee53b6bf2a2c10f8fd4ce
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2022-12-09 15:33:29 +0200

    adding all the nodes files,also all the lateste changes on the site's code

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ const ejs = require("ejs");
 //create variable to handle express function
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+app.get("/signUp", function(req, res) {
+  res.render("signUp");
+});
+
+
 
 //set servere listner on local host
 app.listen(3000, function() {
