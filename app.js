e13a71c54e5abf6286b6f4162ecbba20c0b38a59
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2022-12-11 18:28:52 +0200

    all the files with a lot of changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -10,15 +10,52 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// mongoose.connect("mongodb://127.0.0.1/DriverApp", {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://beniaridani:Daniel112.@cluster0.qj2ixge.mongodb.net/?retryWrites=true&w=majority');
+
+
+const clientScehma = new mongoose.Schema({
+  fname:String,
+  lname:String,
+  userName:String,
+  email:String,
+  password:String
+});
+const Client = mongoose.model("Client",clientScehma);
+
+app.post("/cregister", function(req, res) {
+
+const Newclient = new Client({
+  fname: req.body.firstName,
+  lname:req.body.lastName,
+  userName:req.body.userName,
+  email:req.body.email,
+  password:req.body.password
+});
+  Newclient.save();
+
+// function(res,err){
+//   if(err){
+//     res.redirect("/cregister")
+//   }else{
+//     res.render("/successPage")
+//   }
+// })
+  });
+
 app.get("/", function(req, res) {
   res.render("home");
 });
 
+
 app.get("/login", function(req, res) {
   res.render("login");
 });
-app.get("/register", function(req, res) {
-  res.render("register");
+app.get("/cregister", function(req, res) {
+  res.render("cregister");
+});
+app.get("/dregister", function(req, res) {
+  res.render("dregister");
 });
 app.get("/signUp", function(req, res) {
   res.render("signUp");
